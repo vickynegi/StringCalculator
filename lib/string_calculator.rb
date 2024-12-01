04768c330f10625bb1f99cc0cb04ddaae024efb9
viveknegi111 <v_111negi@yahoo.com>
2024-12-01 21:39:01 +0530

    Allow the add method to calculate the sum if input string contains the delimiter.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,10 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.gsub("\n", ',').split(",").map(&:to_i).sum
+    if numbers.start_with?("//")
+      numbers.gsub("\n", "").split(numbers[2]).map(&:to_i).sum
+    else
+      numbers.gsub("\n", ',').split(",").map(&:to_i).sum
+    end
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,5 +25,9 @@ RSpec.describe StringCalculator do
     it "if input string contains new line character" do
       expect(str_cal_obj.add("1\n2,3")).to eq(6)
     end
+
+    it "if input string contains delimiter" do
+      expect(str_cal_obj.add("//;\n1;2")).to eq(3)
+    end
   end
 end
